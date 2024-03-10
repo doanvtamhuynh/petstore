d071afedb18356cf8a88907c85993d02d8f5b85f
1Cannabis <huynhdoan.2132@gmail.com>
2024-03-10 16:17:03 +0700

    fix

--- qlthucung/qlthucung/qlthucung/Program.cs
+++ qlthucung/qlthucung/qlthucung/Program.cs
@@ -15,7 +15,7 @@ namespace qlthucung
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        //
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
